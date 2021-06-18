09120c7d031a4d67a937880cea26311b71ea5ee2
PaoloFSAcc <paoloagvapro@gmail.com>
2021-06-18 14:24:30 +0200

    Added migrated project Star Match

---
+++ star-match/src/components/App.js
@@ -0,0 +1,11 @@
+// State manager with a custom hook
+import React, { useState } from 'react';
+import Game from './Game';
+
+
+const App = () => {
+	const [gameId, setGameId] = useState(1);
+	return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)}/>;
+}
+
+export default App;
---
+++ star-match/src/renderers/server.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import * as ReactDOMServer from 'react-dom/server';
+
+//import { App } from 'components/App';
+import App from "../components/App";
+
+export async function serverRenderer() {
+  const initialData = {
+    appName: 'Reactful',
+  };
+
+  const pageData = {
+    title: `Hello ${initialData.appName}`,
+  };
+
+  return Promise.resolve({
+    initialData,
+    initialMarkup: ReactDOMServer.renderToString(
+      <App/>,
+    ),
+    pageData,
+  });
+}
